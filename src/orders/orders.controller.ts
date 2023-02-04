d8f00b70788b5b4da0680b1a2f0391c9fa2847fc
Ahmed Salah <75364400+AhmedSa96@users.noreply.github.com>
2023-02-04 17:44:34 +0200

    add guards and swagger tags to orders controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,33 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiOkResponse({ description: 'The record has been successfully created.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
+  @ApiOkResponse({ description: 'The records have been successfully retrieved.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findAll() {
     return this.ordersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @ApiOkResponse({ description: 'The record has been successfully retrieved.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  @ApiOkResponse({ description: 'The record has been successfully updated.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(+id, updateOrderDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @ApiOkResponse({ description: 'The record has been successfully deleted.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   remove(@Param('id') id: string) {
     return this.ordersService.remove(+id);
   }
