ddaaf8ea76bfc57c3eae3cc9b3774cbd959fb4bd
Ahmed Salah <75364400+AhmedSa96@users.noreply.github.com>
2023-02-05 21:25:18 +0200

    create admin guard

---
+++ src/auth/admin.guard.ts
@@ -0,0 +1,23 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserType } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+
+    if (user.user_type !== UserType.ADMIN) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
